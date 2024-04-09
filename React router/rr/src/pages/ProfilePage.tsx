c61bee773dbe042be6fd866d9d9812cac2cc6530
alexprimak58 <aprimak43@gmail.com>
2024-04-09 16:38:28 +0300

    cleared redundant

--- React router/rr/src/pages/ProfilePage.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-function ProfilePage() {
-  const params = useParams<{ profileId: string }>();
-  return <div>ProfilePage {params.profileId}</div>;
-}
-
-export default ProfilePage;
